a9ffc0279ad409ea39caaaa6ba6bc2049677b10c
Nikitosb888bbb <nikitakuzya2007@gmail.com>
2025-11-17 11:21:42 +0300

    Update 17.11.2025_1_prak_6.py

--- 17.11.2025_1_prak_6.py
+++ 17.11.2025_1_prak_6.py
@@ -2,9 +2,9 @@ temp=float(input('Введите температуру тела в Цельси
 pressure = float(input('Введите Ваше верхнее давление: '))
 puls= int(input('Введите свой пульс: '))
 
-if (temp>=36 or temp<=37) and (pressure>=110 or pressure<=130) and (puls>=60 or puls<=100):
+if (temp>=36 or temp<=37) or (pressure>=110 or pressure<=130) or (puls>=60 or puls<=100):
     print ('Все хорошо, никаких действий не требуется.')
-elif ((temp>=35) and (temp<=36) or ((temp>=37) and (temp<=38))) and ((pressure>=105) and (pressure<=110)) or ((pressure>=130) and (pressure <=140)) and ((puls>=55 and puls<=60) or (puls>=100 and puls<=110)):
+elif ((temp>=35) and (temp<=36) or ((temp>=37) and (temp<=38))) or ((pressure>=105) and (pressure<=110)) or ((pressure>=130) and (pressure <=140)) or ((puls>=55 and puls<=60) or (puls>=100 and puls<=110)):
     print ('Отдохните, у Вас легкое недомогание')
 else:
     print ('Требуется помощь врача.')

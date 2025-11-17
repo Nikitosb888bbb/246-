e134ce21c0da3df1155fc9b3dfc7ba738ecac3c0
Nikitosb888bbb <nikitakuzya2007@gmail.com>
2025-11-17 11:45:08 +0300

    Update 17.11.2025_1_prak_6.py

--- 17.11.2025_1_prak_6.py
+++ 17.11.2025_1_prak_6.py
@@ -4,7 +4,8 @@ pulse= int(input('Введите свой пульс: '))
 
 if 36<=temp<=37 and 110<=pressure<=130 and 60<=pulse<=100:
     print('Никаких действий не требуется.')
-elif 35<=temp<=36 and 105<=pressure<=110 and (55<=pulse<=60 or 100<=pulse<=110):
+elif 35<=temp<=36 or 105<=pressure<=110 or (55<=pulse<=60 or 100<=pulse<=110):
     print('Отдохните, у Вас легкое недомогание')
 else:
     print('Необходима помощь врача!')
+

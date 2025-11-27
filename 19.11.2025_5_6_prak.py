51af9517fe4364d70b4db67e4f90d4c24897bf36
Nikitosb888bbb <nikitakuzya2007@gmail.com>
2025-11-27 22:13:38 +0300

    Delete 19.11.2025_5_6_prak.py

--- 19.11.2025_5_6_prak.py
+++
@@ -1,9 +0,0 @@
-vert_1,horiz_1 = map(int,input('Введите положения клетки 1 через пробел (Вертикаль Горизонталь): ').split())
-vert_2,horiz_2 = map(int,input('Введите положение клетки 2 через пробел (Вертикаль Горизонталь): ').split())
-if not (1<=vert_1<=8 and 1<=horiz_1<=8 and 1<=vert_2<=8 and 1<=horiz_2<=8):
-    print('Ошибка! Неправильный ввод данных клеток.')
-else:
-    if vert_1==vert_2 or horiz_1==horiz_2 or abs(horiz_1-horiz_2)==abs(vert_1-vert_2):
-        print('YES. Ферзь может ходить таким образом')
-    else:
-        print('NO. Ферзь не может так ходить')
